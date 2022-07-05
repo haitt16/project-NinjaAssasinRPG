098a0e330a2784ce426c7fc17827cc32441498c7
lekoduckhoi <lekoduckhoi4@gmail.com>
2022-07-05 17:17:28 +0700

    full

--- Java 2D game/main/EventHandler.java
+++ Java 2D game/main/EventHandler.java
@@ -5,18 +5,17 @@ import java.awt.Rectangle;
 public class EventHandler {
 	GamePanel gp;
 	EventRect eventRect[][];
-
+	
 	int previousEventX, previousEventY;
 	boolean canTouchEvent = true;
-
+	
 	public EventHandler(GamePanel gp) {
 		this.gp = gp;
 
-		eventRect = new EventRect[gp.maxWorldCol][gp.MaxWorldRow];
-
+		eventRect = new EventRect[gp.maxScreenCol][gp.maxScreenRow];
 		int col = 0;
 		int row = 0;
-		while(col < gp.maxWorldCol && row < gp.maxWorldRow){
+		while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
 			eventRect[col][row] = new EventRect();
 			eventRect[col][row].x = 23;
 			eventRect[col][row].y = 23;
@@ -26,46 +25,50 @@ public class EventHandler {
 			eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
 
 			col++;
-			if(col == gp.maxWorldCol) {
+			if(col == gp.maxScreenCol) {
 				col = 0;
 				row++;
 			}
 		}
-
-		
 	}
 	public void checkEvent() {
 		
-		// Check if the player charater is more than 1 tile away form the last event
-		int xDistance = Math.abs(gp.player.worldX - previousEventX);
-		int yDistance = Math.abs(gp.player.worldY - previousEventY);
+		// check if the player character is more than 1 tile away from the last event
+		
+		int xDistance = Math.abs(gp.player.x - previousEventX);
+		int yDistance = Math.abs(gp.player.y - previousEventY);
 		int distance = Math.max(xDistance, yDistance);
 		if(distance > gp.tileSize) {
 			canTouchEvent = true;
 		}
-		
+
 		if(canTouchEvent == true) {
-			if (hit(27, 16, "right") == true) {damagePit(27,16,gp.dialogueState);}
-			if (hit(23, 19, "any") == true) {damagePit(27,16,gp.dialogueState);}
-			if (hit(23, 12, "up") == true) {healingPool(23,12,gp.dialogueState);}
-		}
-		
+			if (hit(6, 10, "right") == true) {
+				damagePit(6, 10);
+			}
+			/*if (hit(6, 5, "right") == true) {// Có đoạn code thay thế trong Method pickUpObject, class Player.java  
+				healingPool();
+			}*/
 			
+			if (hit(9, 11, "down") == true) {
+				teleport();
+			}
+		}
 		
 	}
 	public boolean hit(int col, int row, String reqDirection) {
 		boolean hit = false;
 		gp.player.solidArea.x += gp.player.x;
 		gp.player.solidArea.y += gp.player.y;
-		eventRect[col][row].x += col*gp.tileSize + eventRect[col][row].x;
-		eventRect[col][row].y += row*gp.tileSize + eventRect[col][row].y;
+		eventRect[col][row].x += col*gp.tileSize;
+		eventRect[col][row].y += row*gp.tileSize;
 		
-		if(gp.player.solidArea.intersects(eventRect[col][row] && eventRect[col][row].eventDone == false)) {
+		if(gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
 			if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
 				hit = true;
 
-				previousEventX = gp.player.worldX;
-				previousEventY = gp.player.worldY;
+				previousEventX = gp.player.x;
+				previousEventY = gp.player.y;
 			}
 		}
 		
@@ -77,21 +80,19 @@ public class EventHandler {
 		return hit;
 	}
 	
-	public void damagePit(int col, int row, int gameState) {//Bẫy gây dame
-		gp.gameState = gameState;
-		gp.ui.currentDialogue = "You fall into a pit!";
-		gp.player.life -=1;
-//		eventRect[col][row].eventDone = true;
+	public void damagePit(int col, int row) {//Bẫy gây dame
+		gp.ui.showMessage("You fall into a pit");
+		gp.player.life -- ;
+		//eventRect[col][row].eventDone = true;
 		canTouchEvent = false;
 	}
 	
-	public void healingPool(int col, int row, int gameState) {
-		if(gp.keyH.enterPressed == true) {
-			gp.gameState = gameState;
-			gp.ui.currentDialogue = "You drink the water.\nYour life has been recoverd";
-			gp.player.life = gp.player.maxLife;
+	/*public void healingPool() {
+		gp.ui.showMessage("You drink the water.");
+		if (gp.player.life != 6) {
+			gp.player.life++;
 		}
-	}
+	}*/
 	
 	public void teleport() {//Dịch chuyển, phát triển lên có thể thành chuyển map
 		if (gp.player.countChest == 3) {
